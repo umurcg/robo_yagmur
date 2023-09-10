8417e096761ddef370e13956eb090da6fbec602d
Umur Can <>
2023-09-10 13:57:39 +0300

    converted to joystick

--- gamepad_control.py
+++ gamepad_control.py
@@ -5,33 +5,40 @@ from evdev import InputDevice, categorize, ecodes
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 
 # Set up the gamepad device
-gamepad = InputDevice('/dev/input/event2')  # Replace X with the correct number for the DualSense.
+gamepad = InputDevice('/dev/input/event2')
 
-# A mapping of gamepad keys to commands for the robot
-key_map = {
-    ecodes.KEY_UP: 'F',
-    ecodes.KEY_DOWN: 'B',
-    ecodes.KEY_LEFT: 'L',
-    ecodes.KEY_RIGHT: 'R'
-}
+# Initial command to 'Stop'
+prev_command = 'S'
 
 try:
     print("Listening for gamepad input...")
     for event in gamepad.read_loop():
-        # If a key is pressed or released
-        if event.type == ecodes.EV_KEY:
-            key_event = categorize(event)
+        # If a joystick move event occurs
+        if event.type == ecodes.EV_ABS:
 
-            # If the key is pressed and it's in our key map
-            if key_event.keystate == key_event.key_down and key_event.scancode in key_map:
-                command = key_map[key_event.scancode]
+            # Joystick left and right (X-axis)
+            if event.code == ecodes.ABS_X:
+                if event.value < 128:  # Left
+                    command = 'L'
+                elif event.value > 128:  # Right
+                    command = 'R'
+                else:
+                    command = 'S'
+            
+            # Joystick up and down (Y-axis)
+            elif event.code == ecodes.ABS_Y:
+                if event.value < 128:  # Up/Forward
+                    command = 'F'
+                elif event.value > 128:  # Down/Backward
+                    command = 'B'
+                else:
+                    command = 'S'
+
+            # If the new command differs from the previous one, send it to Arduino
+            if command != prev_command:
                 ser.write(command.encode())
                 print(f"Sent command: {command}")
-
-            # If no arrow key is pressed, we assume the robot should stop.
-            elif key_event.keystate == key_event.key_up:
-                ser.write('S'.encode())
-                print("Sent command: Stop")
+                prev_command = command
 
 except KeyboardInterrupt:
     # Close the serial connection
